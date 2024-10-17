d8213ab4aa48329e81efdecef6271263b2c0739b
hsshin <hsshin@euclidsoft.co.kr>
2024-10-17 18:05:01 +0900

    video_call project first commit 20241017 shs

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:video_call/screen/Home.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      theme: ThemeData(
+        fontFamily: 'NotoSans',
+      ),
+      home: Home(),
+    ),
+  );
+}
